dca4de1bc9f3c734fa07bbd4ad5cad771c006d4e
schaoss <ma.gary729@gmail.com>
2019-06-12 18:32:07 +0800

    feat: auto enable

--- execute.js
+++ execute.js
@@ -1,22 +1,8 @@
-const getNews = () => {
-  $.ajax(window.location.href).done(res => {
-    let dom = $.parseHTML(res.substr(res.indexOf('class="ndArticle_margin')))[1]
-    let title = $('.ndArticle_leftColumn>hgroup')
-    $('.ndArticle_leftColumn')
-      .empty()
-      .append(title)
-      .append(dom)
-  })
-}
-
-const onMessage = message => {
-  switch (message.action) {
-    case 'GET':
-      getNews()
-      break
-    default:
-      break
-  }
-}
-
-chrome.runtime.onMessage.addListener(onMessage)
+$.ajax(window.location.href).done(res => {
+  let dom = $.parseHTML(res.substr(res.indexOf('class="ndArticle_margin')))[1]
+  let title = $('.ndArticle_leftColumn>hgroup')
+  $('.ndArticle_leftColumn')
+    .empty()
+    .append(title)
+    .append(dom)
+})
